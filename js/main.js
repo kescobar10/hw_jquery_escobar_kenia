8f5eb4c7f4f2b047e502bc4d8b0f85e7d399f0fc
Kenia Escobar <keniarescobar@gmail.com>
2017-11-17 19:57:47 -0500

    alter style of paragraphs with JQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     var paragraphs = $('p');
+    $(paragraphs).css({
+        'fontSize': '1.1em',
+        'color': '#8f8fa3',
+        'padding': '20px'
+    });
 });
